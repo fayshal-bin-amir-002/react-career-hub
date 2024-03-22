cf82be515adc4dc93c6f506656a965a1da1522e5
fayshal69 <foyshalbinamir@gmail.com>
2024-03-23 02:53:45 +0600

    updated banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,25 @@
+import BannerImg from '../assets/images/user.png'
+import BtnPrimary from './BtnPrimary';
+
 const Banner = () => {
     return (
-        <div>
-            <p>Home page banner</p>
-        </div>
+        <section className="bg-violet-50 dark:dark:text-gray-100 my-24">
+            <div className="container flex flex-col justify-between px-6 mx-auto sm:px-12 lg:px-24 lg:flex-row lg:justify-between">
+                <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                    <h1 className="text-5xl font-bold leading-none sm:text-6xl">One Step Closer To Your <span className='text-violet-400'>Dream Job</span>
+                    </h1>
+                    <p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
+                        <br className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
+                    </p>
+                    <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                        <BtnPrimary name='Get Start'></BtnPrimary>
+                    </div>
+                </div>
+                <div>
+                    <img src={BannerImg} alt="" className="object-contain w-full" />
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,12 +1,36 @@
+import Img1 from '../assets/icons/accounts.png'
+import Img2 from '../assets/icons/creative.png'
+import Img3 from '../assets/icons/marketing.png'
+import Img4 from '../assets/icons/chip.png'
+
 const CategoryList = () => {
     return (
-        <section>
-            <div className="text-center w-1/2 mx-auto mb-8">
+        <section className="mb-24">
+            <div className="text-center w-1/2 mx-auto mb-12">
                 <h1 className="text-5xl font-extrabold mb-4">Job Category List</h1>
                 <p className="font-medium">Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className="grid grid-cols-4 gap-6">
-
+                <div className="bg-violet-50 p-10 rounded-lg">
+                    <img src={Img1} className="mb-8 p-2 bg-violet-200 rounded-lg" />
+                    <h3 className="mb-2 font-bold text-xl">Account & Finance</h3>
+                    <p className="font-medium opacity-60">300 Jobs Available</p>
+                </div>
+                <div className="bg-violet-50 p-10 rounded-lg">
+                    <img src={Img2} className="mb-8 p-2 bg-violet-200 rounded-lg" />
+                    <h3 className="mb-2 font-bold text-xl">Creative Design</h3>
+                    <p className="font-medium opacity-60">100+ Jobs Available</p>
+                </div>
+                <div className="bg-violet-50 p-10 rounded-lg">
+                    <img src={Img3} className="mb-8 p-2 bg-violet-200 rounded-lg" />
+                    <h3 className="mb-2 font-bold text-xl">Marketing & Sales</h3>
+                    <p className="font-medium opacity-60">150 Jobs Available</p>
+                </div>
+                <div className="bg-violet-50 p-10 rounded-lg">
+                    <img src={Img4} className="mb-8 p-2 bg-violet-200 rounded-lg" />
+                    <h3 className="mb-2 font-bold text-xl">Engineering Job</h3>
+                    <p className="font-medium opacity-60">224 Jobs Available</p>
+                </div>
             </div>
         </section>
     );
